9fdb048063de960d146e981e61035992f0d5db10
ESTUDIANTE <estudiante@ul.edu.co>
2019-09-12 20:28:21 -0500

    parcial arturo villa y caterin thomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { GrupoaComponent } from './grupoa/grupoa.component';
+import { GrupobComponent } from './grupob/grupob.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrupoaComponent,
+    GrupobComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import {Routes,RouterModule, Router} from "@angular/router";
+
+import { AppComponent } from './app.component';
+
+
+export const routes: Routes=[
+    {
+        path  : 'grupoa',    
+        component : AppComponent,
+        children: [
+        {
+            path: '',
+            loadChildren:'./grupoa/grupoa.module#GrupoaModule',
+        }
+    ]
+},
+{
+    path  : 'grupob',    
+        component : AppComponent,
+        children: [
+        {
+            path: '',
+            loadChildren:'./grupob/grupob.module#GrupobModule',
+        }
+    ]
+}
+]
+
+
+
+@NgModule({
+imports:[ RouterModule.forRoot(routes,{useHash:true})
+],
+exports:[RouterModule],
+providers:[]
+}) 
+export class AppRoutingModule {}
